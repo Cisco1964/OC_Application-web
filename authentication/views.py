5ec27f534ee2e2d2126215921861b67197fe4ba3
Cisco 1964 <francis.cazeres@gmail.com>
2022-02-11 14:13:14 +0100

    authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from . import forms
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # auto-login user
+            login(request, user)
+            return redirect('logout')
+    return render(request, 'authentication/signup.html', context={'form': form})
